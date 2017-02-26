92a517d6da2fef8863d28fa7618aa2a7f263abdd
sommd <d.j.s.1050@gmail.com>
2017-02-26 19:07:13 +1100

    Fix class/object/interface and update hello.kt

--- hello.kt
+++ hello.kt
@@ -1,5 +1,19 @@
 package hello
 
+interface IGreeter {
+    fun greet()
+}
+
+open class Greeter: IGreeter {
+    companion object {
+        private const val GREETING = "Hello, World!"
+    }
+    
+    override fun greet() {
+        println(GREETING)
+    }
+}
+
 fun main(args: Array<String>) {
-   println("Hello World!")
+   Greeter().greet()
 }
